398ffb4256779b82a236436609f00f950991b06e
Melissa Eaden <melissae@melissaeMBP15tb.local>
2019-04-19 15:39:11 -0500

    updating repo with script, update to 7_4, remove unnecessary files.

---
+++ PNew1/Assets/drop_cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class drop_cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.Instantiate("Cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
